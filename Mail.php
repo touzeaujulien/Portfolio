e0bcbe9139a1d06de6f3549b7fba6bc4b000a7f7
touzeaujulien <touzeau.julien49@gmail.com>
2025-07-07 14:45:21 +0200

    changement index html

--- Mail.php
+++
@@ -1,6 +0,0 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    echo "Votre demande ou question(s) a bien été envoyé !:D ";
-} else {
-    echo "Erreur : La demande n'a pas été envoyé :(";
-}
